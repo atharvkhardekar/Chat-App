dad44893f19a9e5a272e03acf11faa812bce8e2a
atharvkhardekar <khardekaratharv@gmail.com>
2024-12-23 21:39:31 +0530

    Socket, message animation and sound added

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,8 @@ import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
+import { app, server } from "./socket/socket.js";
 
-const app = express();
 const PORT = process.env.PORT || 5000;
 
 dotenv.config(); 
@@ -16,16 +16,17 @@ dotenv.config();
 app.use(express.json()); //To parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
 
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
+
 // app.get("/", (req, res) => {
 //     // root route https://localhost:5000/
 //     res.send("Hello World! I am Atharv");
 // });
 
-app.use("/api/auth", authRoutes);
-app.use("/api/messages", messageRoutes);
-app.use("/api/users", userRoutes);
 
-app.listen(PORT,() => {
+server.listen(PORT,() => {
     connectToMongoDB();
     console.log(`Server Running on port ${PORT}`)
 });
