8c32d6a0e5b0cc50d7e10743b702e6015e5d44c0
atharvkhardekar <khardekaratharv@gmail.com>
2024-12-24 14:33:17 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -11,6 +12,8 @@ import { app, server } from "./socket/socket.js";
 
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 dotenv.config(); 
 
 app.use(express.json()); //To parse the incoming requests with JSON payloads (from req.body)
@@ -20,11 +23,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-// app.get("/", (req, res) => {
-//     // root route https://localhost:5000/
-//     res.send("Hello World! I am Atharv");
-// });
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT,() => {
     connectToMongoDB();
